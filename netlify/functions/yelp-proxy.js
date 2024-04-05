b21724d6fcca60d1ca9ecb92279d4230cb67b612
Марина Романова <marina.simaganova@gmail.com>
2024-04-05 11:54:10 +0200

    new-yelp-function

--- netlify/functions/yelp-proxy.js
+++ netlify/functions/yelp-proxy.js
@@ -2,20 +2,24 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   // Декодирование параметров из строки запроса
-  let { path, term, location, sortBy, categories } = event.queryStringParameters;
-  
-  // Если переданы дополнительные параметры, добавляем их в запрос
-  const params = new URLSearchParams();
-  if (term) params.append('term', term);
-  if (location) params.append('location', location);
-  if (sortBy) params.append('sort_by', sortBy);
-  if (categories) params.append('categories', categories);
+  let { path, term, location, sortBy, categories, businessId } = event.queryStringParameters;
 
-  // Формирование URL для запроса к Yelp API
-  const apiUrl = `https://api.yelp.com/v3/${path}?${params}`;
+  // Определяем, делаем ли мы запрос на детали бизнеса или общий поиск
+  let apiUrl;
+  if (businessId) {
+    // Если запрос касается конкретного бизнеса по ID
+    apiUrl = `https://api.yelp.com/v3/businesses/${businessId}`;
+  } else {
+    // Общий поиск
+    const params = new URLSearchParams();
+    if (term) params.append('term', term);
+    if (location) params.append('location', location);
+    if (sortBy) params.append('sort_by', sortBy);
+    if (categories) params.append('categories', categories);
+    apiUrl = `https://api.yelp.com/v3/${path}?${params}`;
+  }
 
   try {
-    // Выполнение запроса к Yelp API с добавлением необходимого заголовка авторизации
     const yelpResponse = await fetch(apiUrl, {
       headers: {
         Authorization: `Bearer ${process.env.YELP_API_KEY}`, // Использование ключа API Yelp из переменных окружения
@@ -23,16 +27,11 @@ exports.handler = async function(event, context) {
       }
     });
 
-    // Проверка на успешный ответ от API
     if (!yelpResponse.ok) {
-      // Возвращение ошибки, если ответ API не успешен
       throw new Error(`Yelp API responded with a status: ${yelpResponse.statusText}`);
     }
 
-    // Преобразование ответа от API в JSON
     const data = await yelpResponse.json();
-
-    // Возвращение успешного ответа с данными от Yelp API
     return {
       statusCode: 200,
       body: JSON.stringify(data),
@@ -41,7 +40,6 @@ exports.handler = async function(event, context) {
       }
     };
   } catch (error) {
-    // Логирование и возвращение ошибки в случае её возникновения
     console.error('Yelp Proxy Function Error:', error);
     return {
       statusCode: 500,
