107d28acf3cf048aa9e36a0fa9636fb3f5f29238
Марина Романова <marina.simaganova@gmail.com>
2024-04-16 22:30:21 +0200

    footer content

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -17,22 +17,22 @@ const Footer = () => {
                     <p>Khinkali Search</p>
                 </div>
                 <div className="project-description div-column">
-                    <p>This is learning prject...</p>
+                    <p>This is a basic react app that uses Yelp API to allow users to search Georgian eatery in USA cities. This project is part of a off-line challenge from a Codecademy's Frontend developer path.</p>
                     <p className="read-more">Read more on GitHub:</p>
-                    <a href="https://github.com/marina-projects">GitHub</a>
+                    <a href="https://github.com/marina-projects/react_app_georgian_food" target="_blank" rel="noreferrer">GitHub</a>
                 </div>
             </div>
             <div className="second-column div-column">
                 <img src={photo} alt='developer'/>
-                <p>Developer Marina Romanova</p>
+                <p className="developer-name">Developer Marina Romanova</p>
                 <p>Contact me:</p>
-                <p>Email: </p>
-                <p>Website: </p>
+                <a href="mailto: info@marina-romanova.com" target="_blank" rel="noreferrer">info@marina-romanova.com </a>
+                <a href="https://www.marina-romanova.com" target="_blank" rel="noreferrer">www.marina-romanova.com </a>
                 <div className="icons-area div-row">
-                    <img src={linkedin} alt='linkedin-logo' />
-                    <img src={upwork} alt='upwork-logo' />
-                    <img src={whatsapp} alt='whatsapp-logo' />
-                    <img src={telegram} alt='telegram-logo' />
+                    <a href="https://www.linkedin.com/in/yellowmarine/" target="_blank" rel="noreferrer"><img src={linkedin} alt='linkedin-logo' /></a>
+                    <a href="https://www.upwork.com/freelancers/marinar18" target="_blank" rel="noreferrer"><img src={upwork} alt='upwork-logo' /></a>
+                    <a href="https://wa.me/89119996038" target="_blank" rel="noreferrer"><img src={whatsapp} alt='whatsapp-logo' /></a>
+                    <a href="https://t.me/yellowmarine" target="_blank" rel="noreferrer"><img src={telegram} alt='telegram-logo' /></a>
                 </div>
 
             </div>
